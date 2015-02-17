41feab48264f746e8042b2e624877bd7e7513911
DaiyuZhang <gurdjieff100@gmail.com>
2015-02-17 04:06:09 +0000

    finish register

---
+++ app/app.js
@@ -0,0 +1,165 @@
+var app = angular.module('angularProject', ['ngRoute'])
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'app/partials/registration.html',
+        controller: 'registrationController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      })
+  }])
+
+
+    app.filter('cityFilter', function () {
+        return function (data, value) {
+            var filterData = [];
+
+            angular.forEach(data, function (obj) {
+                if (value === "country") {
+                    if (obj.id === "country") {
+                    filterData.push(obj);
+                    }
+                } else {
+                    if (obj.id === "city" && obj.Country === value) {
+                    filterData.push(obj);
+                    }
+                }      
+            })
+           
+
+            console.log(filterData);
+
+            return filterData;
+        }
+    })
+
+    app.directive('even',function(){
+        return {
+            require : 'ngModel',
+            link:function(scope,elm,attrs,ngModelController){
+                ngModelController.$parsers.push(function(viewValue){
+                    if(viewValue % 2 === 0){
+                        ngModelController.$setValidity('even',true);
+                    }else{
+                        ngModelController.$setValidity('even',false);
+                    }
+                    return viewValue;
+                });
+
+//                ngModelController.$formatters.push(function(modelValue){
+//                    return modelValue + 'kittencup';
+//                })
+            }
+        };
+    })
+
+    app.directive('customTextArea',function(){
+        return {
+            restrict:'E',
+            template:'<div contenteditable="true"></div>',
+            replace:true,
+            require : 'ngModel',
+            link:function(scope,elm,attrs,ngModelController){
+
+
+                // view->model
+                elm.on('keyup',function(){
+                    scope.$apply(function(){
+                        ngModelController.$setViewValue(elm.html());
+                    });
+                })
+
+                ngModelController.$render = function(){
+                    elm.html(ngModelController.$viewValue);
+                }
+
+            }
+        };
+    })
+
+    app.factory('LocationService', ['$http' , function($http){
+            var api = {
+                getCities : function() {  // NEW
+                     return $http.get('../localData/irelandCities.json')
+                },
+                getHobbies : function() {  // NEW
+                     return $http.get('../localData/irelandCities.json')
+                }
+            }
+            return api
+        }]);
+
+    app.controller('registrationController', 
+        ['$scope', 'LocationService',
+        function ($scope, LocationService) {
+
+        LocationService.getCities().success(function(data){
+            $scope.cities = data;
+            $scope.countries = []; 
+
+            angular.forEach(data, function(value, key){
+                $scope.countries.push(key);
+            })
+
+            console.log($scope.cities);
+
+        })
+                    // console.log($scope.data);
+
+        var that = this;
+
+        $scope.hobbies = [
+            {
+                id: 1,
+                name: 'play'
+            },
+            {
+                id: 2,
+                name: 'watch TV '
+            },
+            {
+                id: 3,
+                name: 'reading'
+            },
+        ];
+
+      
+
+
+        $scope.data = {
+            hobbies: [1, 2],
+            city: 3
+        };
+
+
+        $scope.origData = angular.copy($scope.data);
+
+        $scope.reset = function(){
+
+            // $scope.data = angular.copy($scope.origData);
+            // that.initCity();
+            $scope.myForm.$setPristine();
+        }
+
+
+        
+
+        $scope.toggleHobbySelection = function (id) {
+
+            var index = -1;
+            if ($scope.data.hobbies === undefined) {
+                $scope.data.hobbies = [];
+            } else {
+                index = $scope.data.hobbies.indexOf(id);
+            }
+
+            if (index === -1) {
+                $scope.data.hobbies.push(id);
+            } else {
+                $scope.data.hobbies.splice(index, 1);
+            }
+
+        }
+    }]);
